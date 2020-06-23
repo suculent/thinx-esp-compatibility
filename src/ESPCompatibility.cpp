7f4acffd40ba9d26fc754f29018d3df07ad313c6
Matej Sychra <suculent@me.com>
2020-06-23 11:27:42 +0200

    update with regards to https://github.com/esp8266/Arduino/issues/2309

--- src/ESPCompatibility.cpp
+++ src/ESPCompatibility.cpp
@@ -10,6 +10,19 @@ char * ESPCompatibility::get_mac_id()
 {
   uint8_t raw[6];
 
+  /*
+  uint32_t mac0 = MAC0;
+  uint32_t mac1 = MAC1;
+  uint32_t chipid = CHIPID;
+  uint32_t apiChipId = ESP.getChipId();
+  String macAddress = WiFi.macAddress();
+  Serial.printf("mac0 =       0x%08x\n", mac0);
+  Serial.printf("mac1 =       0x%08x\n", mac1);
+  Serial.printf("chipid =     0x%08x\n", chipid);
+  Serial.printf("apiChipId =  0x%08x\n", apiChipId);
+  Serial.printf("macaddress = %s\n", macAddress.c_str());
+  */
+
 #ifdef ESP8266
   uint32_t chipid = ESP.getChipId();
   sprintf(this->mac, "5CCF7F%.6X", ESP.getChipId());
@@ -19,6 +32,11 @@ char * ESPCompatibility::get_mac_id()
   esp_read_mac(raw, ESP_MAC_WIFI_STA);
   sprintf(this->mac, "%02X%02X%02X%02X%02X%02X", raw[0], raw[1], raw[2], raw[3], raw[4], raw[5]);
 #endif
+
+  String macAddress = WiFi.macAddress();
+  macAddress.replace(":", "");
+  macAddress.replace(" ", "");
+  sprintf(this->mac, "%s", macAddress.c_str());
 
   return this->mac;
 }
